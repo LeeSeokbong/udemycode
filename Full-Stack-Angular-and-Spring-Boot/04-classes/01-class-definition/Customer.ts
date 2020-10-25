d3367ee0ff859be7849b4df823d124579c431fd7
LeeSeokBong <kingstar6035@gmail.com>
2020-10-25 17:55:54 +0900

    폴더명 변경

---
+++ Full-Stack-Angular-and-Spring-Boot/04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,15 @@
+class Customer {
+
+    firstName: string;
+    lastName: string;
+
+}
+
+// let's create an instance
+let myCustomer = new Customer();
+
+myCustomer.firstName = "Leo";
+myCustomer.lastName = "Lee";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
